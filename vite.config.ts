c6a3400d3ebb7280ff404930e8236251b7f28a1d
Isaac <48893799+IsaacZM@users.noreply.github.com>
2022-06-29 19:01:23 +0200

    Added plugin generateThumb

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite'
-
-import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
+import react from '@vitejs/plugin-react'
+import axios from 'axios'
+
+const generateThumbPlugin = () => {
+    return {
+        name: 'generate-thumb',
+        async transformIndexHtml(html: string) {
+            const request = await axios.get('https://api.atlasacademy.io/export/JP/basic_equip_lang_en.json')
+            const response = request.data
+            const thumb = response.pop().face
+            return html.replace(/\{face\}/, thumb.replace('.png', '_bordered.png'))
+        },
+    }
+}
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({})
-  ]
+    plugins: [react(), VitePWA({}), generateThumbPlugin()],
 })
